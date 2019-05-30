2b554b1b9f893801841486337f9d3014dfbf98c2
João Marins <jgcmarins@gmail.com>
2019-05-30 16:16:13 -0300

    test(class-counter): add basic example of testing a counter

--- examples/src/components/class/Class.tsx
+++ examples/src/components/class/Class.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ClassContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default class Class extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  incrementCount = () => this.setState({ counter: this.state.counter + 1 });
+
   render() {
+    const { counter } = this.state;
     return (
-      <div><span data-testid="class" >class</span></div>
+      <ClassContainer>
+        <span data-testid="counter-head" >Counter</span>
+        <span data-testid="counter-value" >{counter}</span>
+        <button data-testid="counter-button" onClick={this.incrementCount} >Count</button>
+      </ClassContainer>
     );
   }
 }
--- examples/src/components/class/__tests__/Class.test.tsx
+++ examples/src/components/class/__tests__/Class.test.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
-import { render, waitForElement} from '@testing-library/react';
+import { render, waitForElement, fireEvent } from '@testing-library/react';
 
 import Class from '../Class';
 
 describe('Class Tests', () => {
-  it('should render Class Component', async () => {
-    const { getByTestId } = render(<Class/>);
+  const { getByTestId, getByText, container } = render(<Class/>);
 
+  it('should render Class Component', async () => {
     const spanTextNode = await waitForElement(() =>
-      getByTestId('class'),
+      getByTestId('counter-head'),
     );
 
-    expect(spanTextNode.textContent).toBe('class');
-  })
+    expect(spanTextNode.textContent).toBe('Counter');
+  });
+
+  it('should check count value on mount', async () => {
+    const spanTextNode = await waitForElement(() =>
+      getByTestId('counter-value'),
+    );
+
+    expect(spanTextNode.textContent).toBe('0');
+  });
+
+  it('should click and assert counter value', async () => {
+    fireEvent.click(getByText('Count'));
+
+    const spanTextNode = await waitForElement(() =>
+      getByTestId('counter-value'),
+    );
+
+    expect(spanTextNode.textContent).toBe('1');
+  });
 });
